38116fbcf9055f191b2b2f2185f51d41937166ba
hbhhb7777 <andreab6765@gmail.com>
2024-12-06 10:06:39 +0100

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,7 +1,7 @@
 export async function onRequest({ request }) {
 	const url = new URL(request.url)
 
-	const proxyUrl = 'https://bildingfast.monster'
+	const proxyUrl = 'https://vespersfort.monster'
 	const backendUrl = `${proxyUrl}${url.pathname}`
 
 	const response = await fetch(backendUrl, {
