1bfa66d7b74b40e1dcbcfdbe19e55355704a0260
hbhhb7777 <andreab6765@gmail.com>
2024-12-12 09:35:37 +0100

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,7 +1,7 @@
 export async function onRequest({ request }) {
 	const url = new URL(request.url)
 
-	const proxyUrl = 'https://casualsfds.monster'
+	const proxyUrl = 'https://kilomanok.monster'
 	const backendUrl = `${proxyUrl}${url.pathname}`
 
 	const response = await fetch(backendUrl, {
