8b83a4bb16938ee9c203424957cfadcb29c961e5
hbhhb7777 <andreab6765@gmail.com>
2024-12-16 15:48:42 +0100

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,7 +1,7 @@
 export async function onRequest({ request }) {
 	const url = new URL(request.url)
 
-	const proxyUrl = 'https://kilomanok.monster'
+	const proxyUrl = 'https://visionzas.monster'
 	const backendUrl = `${proxyUrl}${url.pathname}`
 
 	const response = await fetch(backendUrl, {
